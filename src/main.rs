96345767288303461d82a4d37ac4819eb9caa1d5
appare45 <kaibatsu35.7m45@gmail.com>
2025-08-08 09:06:46 +0900

    red

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 // https://github.com/rust-lang/rust/issues/106655
 #![feature(offset_of)]
 use core::cmp::min;
-use core::{arch::asm, mem::offset_of, mem::size_of, panic::PanicInfo, ptr::null_mut };
+use core::{arch::asm, mem::offset_of, mem::size_of, panic::PanicInfo, ptr::null_mut};
 
 type EfiVoid = u8;
 type EfiHandle = u64;
@@ -98,6 +98,15 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
             }
         }
     }
+
+    for y in 0..vram.height() / 2 {
+        for x in 0..vram.width() / 2 {
+            if let Some(pixel) = vram.pixel_at_mut(x, y) {
+                *pixel = 0xff0000;
+            }
+        }
+    }
+
     loop {
         hlt()
     }
@@ -153,7 +162,7 @@ struct VramBufferInfo {
     buf: *mut u8,
     width: i64,
     height: i64,
-    pixels_per_line: i64
+    pixels_per_line: i64,
 }
 
 impl Bitmap for VramBufferInfo {
@@ -180,5 +189,10 @@ impl Bitmap for VramBufferInfo {
 
 fn init_vram(efi_system_table: &EfiSystemTable) -> Result<VramBufferInfo> {
     let gp = locate_graphic_protocol(efi_system_table)?;
-    Ok(VramBufferInfo { buf: gp.mode.frame_buffer_base as *mut u8, width: gp.mode.info.horizontal_resolution as i64, height: gp.mode.info.vertical_resolution as i64, pixels_per_line: gp.mode.info.pixels_per_scan_line as i64 })
+    Ok(VramBufferInfo {
+        buf: gp.mode.frame_buffer_base as *mut u8,
+        width: gp.mode.info.horizontal_resolution as i64,
+        height: gp.mode.info.vertical_resolution as i64,
+        pixels_per_line: gp.mode.info.pixels_per_scan_line as i64,
+    })
 }
