f7d6151889266eedad5640e85bf2ac226773f401
appare45 <kaibatsu35.7m45@gmail.com>
2025-09-02 21:33:48 +0900

    コード修正

--- src/init.rs
+++ src/init.rs
@@ -3,6 +3,7 @@ use crate::uefi::exit_from_efi_boot_services;
 use crate::uefi::EfiHandle;
 use crate::uefi::EfiSystemTable;
 use crate::uefi::MemoryMapHolder;
+// use crate::x86::PML4;
 
 pub fn init_basic_runtime(
     image_handle: EfiHandle,
@@ -13,3 +14,7 @@ pub fn init_basic_runtime(
     ALLOCATOR.init_with_mmap(&memory_map);
     memory_map
 }
+
+// pub fn init_paging(memory_map: &MemoryMapHolder) {
+//     let mut table = PML4::new();
+// }
